5a01baa35eea8a1b16baddc1d03eda488e4daff3
Alperen <alperenduran@outlook.com>
2020-08-08 11:55:46 +0300

    Arrange colors.

--- Credit Card Store/SceneDelegate.swift
+++ Credit Card Store/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = NavigationController(root: ViewController())
+        let controller = ViewController()
+        let navigationController = NavigationController(root: controller)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
